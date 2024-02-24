74a06cc6141779e97f07bf26c852f297c462ffe5
anhtv08 <anhtv010888@gmail.com>
2024-02-24 15:43:54 +0800

    adding factory pattern

--- src/command_pattern/Light.java
+++ src/command_pattern/Light.java
@@ -1,5 +1,10 @@
 package command_pattern;
 
+/*
+ * 
+ * reciever in the comnand pattern
+ * this will be execute by commander sometime
+ */
 public class Light {
     public void On(){
         System.out.println("turn light on");
