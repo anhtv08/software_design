1f2e3b8466a45fbb1b5cf6019b7efb4c205946a9
anhtv08 <anhtv010888@gmail.com>
2024-02-24 12:08:25 +0800

    add change

--- src/observer_pattern/WeatherStationTestDriver.java
+++ src/observer_pattern/WeatherStationTestDriver.java
@@ -3,6 +3,12 @@ package observer_pattern;
 /**
  * 
  * this testing driver for pub/sub design pattern
+ *  simple test for observer pattern/
+ *  1. subject which observerable
+ *  2. subject allow list of client/consumer to subcribe to it
+ *  3. also, client can leave subcription form subject and ignore and change from subject
+ *  4. any change state from subject, will notify all client and consumer
+ *  5. consumer/client will perform the data update
  */
 public class WeatherStationTestDriver {
     public static void main(String[] args) throws Exception {
