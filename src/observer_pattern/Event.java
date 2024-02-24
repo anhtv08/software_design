d4b82d7b4dc26689a8cd2964c377fd3e4e43f724
anhtv08 <anhtv010888@gmail.com>
2024-02-24 12:28:48 +0800

    add header to message

--- src/observer_pattern/Event.java
+++ src/observer_pattern/Event.java
@@ -1,10 +1,24 @@
 package observer_pattern;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /*
   event data to encapsulate the state change
 */
 public class Event<T> {
 
+    // header is meta data for message
+    // the meta data will be used for data management, rounting
+    Map <String, String>headers = new HashMap<>();
+    
+    // payload of message
     T data;
+
+    public Event(Map<String, String> headers, T data) {
+        this.headers = headers;
+        this.data = data;
+    }
 
     public Event(T data) {
         this.data = data;
@@ -14,4 +28,13 @@ public class Event<T> {
     public T getData() {
         return data;
     }
+
+    public void addHeader(
+        String key,
+        String value
+
+    ){
+        headers.put(key, value);
+    }
+    
 } 
